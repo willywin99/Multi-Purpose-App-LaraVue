73134a3eb0b5d3317f056b281049f03a22db76a3
willywin99 <willywinsianturi@gmail.com>
2020-12-11 13:59:45 +0700

    Detect Active Menu in Vue Router and Laravel Plus HTML5 History Mode

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Route::get('{path}', 'HomeController@index')->where('path', '([A-z\d-\/_.]+)?');
+
+Route::get('/{any}', 'HomeController@index')->where('any', '.*');
+
+
