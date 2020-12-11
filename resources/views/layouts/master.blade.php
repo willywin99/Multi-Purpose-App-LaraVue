5ea4acb9c25515533ec54024eb33ef4633329caf
willywin99 <willywinsianturi@gmail.com>
2020-12-11 22:43:31 +0700

    Custom Color for Font Awesome Icons and How Logout Works in Laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -194,7 +194,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                with font-awesome or any other icon font library -->
             <li class="nav-item">
                 <router-link to="/dashboard" class="nav-link">
-                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <i class="nav-icon fas fa-tachometer-alt blue"></i>
                     <p>
                     Dashboard
                     
@@ -204,7 +204,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             
           <li class="nav-item">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-cog"></i>
+              <i class="nav-icon fas fa-cog green"></i>
               <p>
                 Management
                 <i class="right fas fa-angle-left"></i>
@@ -228,7 +228,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
           <li class="nav-item">
             <router-link to="/profile" class="nav-link">
-                <i class="nav-icon fas fa-user"></i>
+                <i class="nav-icon fas fa-user orange"></i>
                 <p>
                 Profile
                 
@@ -237,13 +237,27 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
-            <a href="#" class="nav-link">
+            {{-- <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-power-off"></i>
                 <p>
-                Logout
+                  Logout
                 
                 </p>
+            </a> --}}
+
+            <a class="nav-link" href="{{ route('logout') }}"
+              onclick="event.preventDefault();
+                  document.getElementById('logout-form').submit();">
+              <i class="nav-icon fas fa-power-off red"></i>
+              <p>
+                {{ __('Logout') }}
+              </p>
             </a>
+
+            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                @csrf
+            </form>
+
           </li>
 
           {{-- <li class="nav-item">
