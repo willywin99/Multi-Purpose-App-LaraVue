fa12e7ef92a7f8f6ed76b143df4b06cbf19f8568
willywin99 <willywinsianturi@gmail.com>
2020-12-28 04:28:53 +0700

    Send HTTP Request Every 3 Seconds to Update Data in Laravel and VueJs

--- public/js/app.js
+++ public/js/app.js
@@ -2017,7 +2017,12 @@ __webpack_require__.r(__webpack_exports__);
     }
   },
   created: function created() {
+    var _this2 = this;
+
     this.loadUsers();
+    setInterval(function () {
+      return _this2.loadUsers();
+    }, 3000);
   }
 });
 
--- resources/js/components/Users.vue
+++ resources/js/components/Users.vue
@@ -199,6 +199,7 @@
         },
         created() {
             this.loadUsers();
+            setInterval(() => this.loadUsers(), 3000);
         }
     }
 </script>
