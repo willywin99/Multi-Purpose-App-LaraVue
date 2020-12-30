cc3d6108b78ec5049d4ed429c076e173e0b47f1e
willywin99 <willywinsianturi@gmail.com>
2020-12-30 07:57:22 +0700

    Installing Laravel Passport for API Auth in Laravel

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -237,6 +237,15 @@ scratch. This page gets rid of all links and provides the needed markup only.
           </li>
 
           <li class="nav-item">
+            <router-link to="/developer" class="nav-link">
+                <i class="nav-icon fas fa-cogs"></i>
+                <p>
+                    Developer
+                </p>
+            </router-link>
+          </li>
+
+          <li class="nav-item">
             {{-- <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-power-off"></i>
                 <p>
