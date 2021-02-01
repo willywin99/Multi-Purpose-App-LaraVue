3e797238b785cbc26c64e9c53f0c5ef62da453e0
willywin99 <willywinsianturi@gmail.com>
2021-02-02 01:23:54 +0700

    Easily Add ACL to Laravel without Extra Tables in Database

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -235,7 +235,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </p>
             </router-link>
           </li>
-
+          @can('isAdmin')
           <li class="nav-item">
             <router-link to="/developer" class="nav-link">
                 <i class="nav-icon fas fa-cogs"></i>
@@ -244,7 +244,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                 </p>
             </router-link>
           </li>
-
+          @endcan
           <li class="nav-item">
             {{-- <a href="#" class="nav-link">
                 <i class="nav-icon fas fa-power-off"></i>
